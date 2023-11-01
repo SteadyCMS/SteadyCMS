28e84b73ae1e33f2df77dee7197b1ac5d6f0ed3f
Noah Rahm <correctsyntax@yahoo.com>
2023-10-31 22:52:19 -0500

    Update upload dialog according to mockup

--- components/uploadDialog.vue
+++ components/uploadDialog.vue
@@ -7,6 +7,8 @@
 
   import AccentButton from '../components/buttons/AccentButton.vue';
 
+  import UploadIcon from '../components/icons/UploadIcon.vue';
+
   const steadyAPI = SteadyAPI();
   const generalStore = useGeneralStore();
 
@@ -17,28 +19,17 @@
     declineText: "Close",
     cancelText: "Cancel"
   });
-
-
-
 </script>
+
 <template>
-<SimpleModal :title='props.title' size="md" >
-<div class="flex grow">
-
-
-      <div class="flex flex-col border-dashed border-2 border-gray-400 rounded-sm m-5 p-20 content-center grow">
-
-       <h1 class="font-bold mx-auto">Drop files to upload into media libary</h1> 
-       <span class="tent-sm text-gray-400  mx-auto">or</span>
-      <!-- <img :src="currentImage" :alt="currentImage" width="200" height="100">  -->
-
-
-      <AccentButton text="Select files" @click="confirmModal({accepted: true })" class="font-bold mx-auto bg-black text-white" /> 
+  <SimpleModal :title='props.title' size="md" >
+    <div class="flex grow bg-tint">
+      <div class="flex flex-col border-dashed border-2 border-tint-6 bg-white rounded-sm m-5 p-20 content-center grow">
+        <UploadIcon class="w-16 h-16 fill-tint-10 mx-auto"/>
+        <h1 class="font-semibold text-tint-10 mx-auto mt-4 text-lg">Drop file to upload into media library</h1> 
+        <span class="tent-sm text-tint-9 mx-auto">or</span>
+        <AccentButton text="Select file" @click="confirmModal({accepted: true })" class="mt-2 py-2 px-4 text-white hover:text-white/80 bg-black hover:bg-black text-sm font-semibold rounded-lg ease-in-out duration-300 w-fit mx-auto" /> 
       </div>
-
-</div>
-  <template #footer>
-  </template>
-</SimpleModal>
-
+    </div>
+  </SimpleModal>
 </template>

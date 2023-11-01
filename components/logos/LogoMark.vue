9f0e9f9e8d170730bacae2a905ad0e9cc466bb3c
Noah Rahm <correctsyntax@yahoo.com>
2023-10-31 19:03:11 -0500

    Update dashboard sidebar according to mockup

--- components/logos/LogoMark.vue
+++ components/logos/LogoMark.vue
@@ -1,3 +1,8 @@
 <template>
-  <img src="assets/images/steady-cms-logomark.png"/>
+  <svg viewBox="0 0 140 140" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <g id="Group-4">
+      <path d="M140 75.7377L89.7152 75.7377L115.405 121.639L0 121.639L0 140L140 140L140 75.7377Z" id="Rectangle-2-Copy-2-Subtract" fill="#29C2E1" fill-rule="evenodd" stroke="none" />
+      <path d="M140 0L0 0L0 64.2624L49.0002 64.2624L24.5946 20.6557L139.992 20.6557L139.992 64.2624L140 64.2624L140 0Z" id="Rectangle-2-Copy-3-Subtract" fill="#FFFFFF" fill-rule="evenodd" stroke="none" />
+    </g>
+  </svg>
 </template>
--- views/AppDashboard.vue
+++ views/AppDashboard.vue
@@ -94,82 +94,86 @@
 </script>
 
 <template>
-  <main> 
-    <div class="flex flex-row">
+  <div class="flex flex-row"> 
 
-      <div class="h-screen w-1/3 lg:w-1/4 lg:max-w-xs bg-black px-8 py-6">
-        <!-- Logo -->
-        <div class="flex justify-between mb-4">
-          <LogoLight class="h-8 w-auto" />
-          <button class="py-1.5 px-2 border border-black hover:border-tint-10 rounded-lg duration-300">
-            <IconThreeDots class="w-4 h-4 fill-tint-4" />
-          </button>
+    <aside class="relative h-screen max-w-xs w-2/5 bg-black lg:w-1/4">
+      <div class="flex flex-row items-center justify-between border-b border-tint-10 px-6 py-2">
+        <div class="flex h-12 w-12 items-center justify-center rounded-full border border-accent-glow/50">
+          <LogoMark class="w-5 h-5" />
         </div>
-        <div class="flex flex-row space-x-1 mb-8">
-          <div class="relative w-full">
+        <button class="rounded-lg border border-transparent p-1 ease-in-out duration-300 hover:border-tint-10">
+          <svg class="h-6 w-6 fill-white/80" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M140,128a12,12,0,1,1-12-12A12,12,0,0,1,140,128Zm56-12a12,12,0,1,0,12,12A12,12,0,0,0,196,116ZM60,116a12,12,0,1,0,12,12A12,12,0,0,0,60,116Z"></path></svg>
+        </button>
+      </div>
+      <div class="px-6">
+        <div class="flex flex-row space-x-2 mt-5">
+          <div class="relative w-full min-w-0">
             <!-- Website selector -->
-            <button class="flex flex-grow items-center justify-between px-4 py-2 rounded-lg border border-tint-10 w-full hover:bg-accent-glow duration-300" 
-              @click="dropdownState =! dropdownState">
-              <div class="flex items-center space-x-2">
-                <LogoMark class="w-5 h-5 border border-tint-10 rounded" />
-                <span class="text-white text-sm font-semibold">{{ currentWebsite }}</span>
+            <button @click="dropdownState =! dropdownState" class="flex w-full flex-grow items-center justify-between rounded-lg border border-tint-10 px-3 py-2 ease-in-out duration-300 hover:bg-accent-glow">
+              <div class="flex max-w-xs items-center space-x-3 overflow-hidden">
+                <img class="h-5 w-5 rounded-sm" src="https://picsum.photos/200" alt="" />
+                <p class="max-w-xs overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold text-white">
+                  {{ currentWebsite }}
+                </p>
               </div>
-
-              <IconArrowDown class="fill-tint-4 w-3 h-3 ml-1" :class="{'rotate-180 duration-300': dropdownState, 'duration-300' : !dropdownState}"/>
+              <IconArrowDown class="fill-white w-3 h-3 ml-1" :class="{'rotate-180 duration-300': dropdownState, 'duration-300' : !dropdownState}"/>
             </button>
-            <!-- Dropdown menu -->
-            <div class="absolute top-6 left-0 bg-black border border-tint-10 text-base z-50 list-none rounded-lg my-4 w-full" id="dropdown" :class="{'opacity-100': dropdownState, 'visible': dropdownState, 'opacity-0': !dropdownState, 'hidden': !dropdownState}">
-              <ul class="py-1" aria-labelledby="dropdown">
-                <li v-for="site in websites" :key="site.path" @click="changeCurrentWebsite(site.path)" class="flex flex-row flex-grow rounded-lg hover:bg-accent-glow hover:cursor-pointer px-4 py-2 duration-300">
-                  <div class="flex flex-row items-center space-x-2">
-                    <LogoMark class="w-5 h-5 border border-tint-10 rounded" />
-                    <span class="text-white text-sm font-bold"> {{ site.name }}</span>
+            <div class="absolute left-0 top-7 z-50 my-4 w-full list-none rounded-lg border border-tint-10 bg-black text-base" id="dropdown" :class="{'opacity-100': dropdownState, 'visible': dropdownState, 'opacity-0': !dropdownState, 'hidden': !dropdownState}">
+              <ul class="space-y-1 py-1 w-full" aria-labelledby="dropdown">
+                <li v-for="site in websites" :key="site.path" @click="changeCurrentWebsite(site.path)" class="w-full cursor-pointer rounded-md px-3 py-2 ease-in-out duration-300 hover:bg-accent-glow">
+                  <div class="flex max-w-xs items-center space-x-3 overflow-hidden">
+                    <img class="h-5 w-5 rounded-sm" src="https://picsum.photos/200?seed=32" alt="" />
+                    <p class="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold text-white">
+                      {{ site.name }}
+                    </p>
                   </div>
                 </li>
+                <!-- New website btn -->
+                <li @click="createNewWebsite(true)" class="cursor-pointer rounded-md px-3 py-2 ease-in-out duration-300 hover:bg-accent-glow">
+                  <button class="flex items-center space-x-3">
+                    <svg class="h-5 w-5 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+                      <path d="M128,80,98.13,102.4a8,8,0,0,1-4.8,1.6H32V64a8,8,0,0,1,8-8H93.33a8,8,0,0,1,4.8,1.6Z" opacity="0.2"></path>
+                      <path d="M216,72H130.67L102.93,51.2a16.12,16.12,0,0,0-9.6-3.2H40A16,16,0,0,0,24,64V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V88A16,16,0,0,0,216,72ZM40,64H93.33l21.34,16L93.33,96H40ZM216,200H40V112H93.33a16.12,16.12,0,0,0,9.6-3.2L130.67,88H216Zm-80-72v16h16a8,8,0,0,1,0,16H136v16a8,8,0,0,1-16,0V160H104a8,8,0,0,1,0-16h16V128a8,8,0,0,1,16,0Z"></path>
+                    </svg>
+                    <p class="text-sm font-semibold text-white">New website</p>
+                  </button>
+                </li>
               </ul>
-            </div> 
+            </div>
           </div>
-          <!-- New website button -->
-          <button @click="createNewWebsite(true)" class="p-1.5 border border-tint-10 rounded-lg hover:bg-accent-glow duration-300">
-            <IconPlus class="fill-tint-4 w-5 h-5" />
+          <!-- Preview website btn -->
+          <button class="relative py-1.5 px-2.5 border border-tint-10 rounded-lg hover:bg-accent-glow ease-in-out duration-300">
+            <svg class="relative w-4 h-4 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M224,104a8,8,0,0,1-16,0V59.32l-66.33,66.34a8,8,0,0,1-11.32-11.32L196.68,48H152a8,8,0,0,1,0-16h64a8,8,0,0,1,8,8Zm-40,24a8,8,0,0,0-8,8v72H48V80h72a8,8,0,0,0,0-16H48A16,16,0,0,0,32,80V208a16,16,0,0,0,16,16H176a16,16,0,0,0,16-16V136A8,8,0,0,0,184,128Z"></path></svg>
           </button>
         </div>
-        <!-- Page links -->
-        <div class="text-white flex flex-col space-y-1">
-          <RouterLink class="flex items-center rounded-lg px-4 py-2 text-tint-2 fill-tint-3 text-sm font-semibold"
+        <div class="flex flex-col space-y-1 mt-5">
+          <RouterLink class="flex items-center rounded-lg px-3 py-2 text-tint-2 fill-tint-3 text-sm font-semibold hover:text-tint-2/90 ease-in-out duration-300"
             active-class="active-rl"
             to="/posts">
             <IconPosts class="w-4 h-4 mr-2" /> Posts
           </RouterLink>
-          <RouterLink class="flex items-center rounded-lg px-4 py-2 text-tint-2 fill-tint-3 text-sm font-semibold"
+          <RouterLink class="flex items-center rounded-lg px-3 py-2 text-tint-2 fill-tint-3 text-sm font-semibold hover:text-tint-2/90 ease-in-out duration-300"
             active-class="active-rl" 
             to="/pages">
             <IconPages class="w-4 h-4 mr-2" /> Pages
           </RouterLink>
-          <RouterLink class="flex items-center rounded-lg px-4 py-2 text-tint-2 fill-tint-3 text-sm font-semibold" 
+          <RouterLink class="flex items-center rounded-lg px-3 py-2 text-tint-2 fill-tint-3 text-sm font-semibold hover:text-tint-2/90 ease-in-out duration-300" 
             active-class="active-rl"
             to="/tags">
             <IconTags class="w-4 h-4 mr-2" /> Tags
           </RouterLink>
-          <RouterLink class="flex items-center rounded-lg px-4 py-2 text-tint-2 fill-tint-3 text-sm font-semibold" 
+          <RouterLink class="flex items-center rounded-lg px-3 py-2 text-tint-2 fill-tint-3 text-sm font-semibold hover:text-tint-2/90 ease-in-out duration-300" 
             active-class="active-rl"
             to="/media">
             <IconTags class="w-4 h-4 mr-2" /> Media
           </RouterLink>
-
-          <span class="h-px w-full bg-gray-900 my-2"></span>
-          <RouterLink class="flex items-center rounded-lg px-4 py-2 text-tint-2 fill-tint-3 text-sm font-semibol" 
-            active-class="active-rl"
-            to="/settings">
-            <IconTags class="w-4 h-4 mr-2" /> Settings
-          </RouterLink>
         </div>
       </div>
+    </aside>
 
-      <div class="flex h-screen w-3/4">
-        <RouterView />
-      </div>
+    <main class="flex h-screen w-3/5 lg:w-3/4">
+      <RouterView />
+    </main>
 
-    </div>
-  </main>
+  </div>
 </template>

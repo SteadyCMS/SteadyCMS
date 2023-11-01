3bceea2ce4d9f449514cc87c5362bbdcc6f1c88e
Noah Rahm <correctsyntax@yahoo.com>
2023-11-01 18:06:17 -0500

    Fix image block

--- components/blocks/ImageBlock.vue
+++ components/blocks/ImageBlock.vue
@@ -32,21 +32,15 @@
 <template>
   <div class="flex flex-col">
     <div class="flex flex-col">
-      <span @click="showMediaDialog" class="mt-1 cursor-pointer flex flex-row space-x-2 items-center py-2 px-4 text-white hover:text-white/80 fill-white hover:fill-white/80 bg-black hover:bg-black text-sm font-semibold rounded-lg ease-in-out duration-300 w-fit">
-       <ImagesSquareIcon class="w-5 h-5 mr-1"/>
-       Select image
-       </span>
       <img :src="props.item.src">
+      <button @click="showMediaDialog" class="mt-1 cursor-pointer flex flex-row space-x-2 items-center py-2 px-4 text-white hover:text-white/80 fill-white hover:fill-white/80 bg-black hover:bg-black text-sm font-medium rounded-lg ease-in-out duration-300 w-fit">
+        <ImagesSquareIcon class="w-5 h-5 mr-1"/>
+        Select image
+      </button>
     </div>
-    <input type="text" placeholder="Type caption..." v-model="props.item.caption" 
-    class="mt-1 px-2 py-1 block italic w-full bg-white outline-none border-0 border-none text-sm placeholder-tint-6 placeholder:italic"/>
-</div>
+    <input type="text" placeholder="Type caption..." v-model="props.item.caption" class="mt-1 px-2 py-1 block italic w-full bg-white outline-none border-0 border-none text-sm placeholder-tint-6 placeholder:italic"/>
+  </div>
 </template>
-
-
-
-
-
 
 <!--
     {{< figure src="/posts/racoons.jpg" alt="Racoons" caption="We caught a family of racoons raiding our kitchen area" >}}
@@ -61,18 +55,3 @@
        "focus": false 
     },
 --> 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

00555a4c2d384f6194b575829eb66f5d05e3b1a9
Noah Rahm <correctsyntax@yahoo.com>
2023-11-09 14:48:39 -0600

    Fix icons

--- components/blockTopbar/HeaderBlockTopbar.vue
+++ components/blockTopbar/HeaderBlockTopbar.vue
@@ -1,9 +1,9 @@
 <script setup>
-  import h2Icon from '../icons/headerIcons/h2Icon.vue';
-  import h3Icon from '../icons/headerIcons/h3Icon.vue';
-  import h4Icon from '../icons/headerIcons/h4Icon.vue';
-  import h5Icon from '../icons/headerIcons/h5Icon.vue';
-  import h6Icon from '../icons/headerIcons/h6Icon.vue';
+  import H2Icon from '../icons/HeadingIcons/H2Icon.vue';
+  import H3Icon from '../icons/HeadingIcons/H3Icon.vue';
+  import H4Icon from '../icons/HeadingIcons/H4Icon.vue';
+  import H5Icon from '../icons/HeadingIcons/H5Icon.vue';
+  import H6Icon from '../icons/HeadingIcons/H6Icon.vue';
 
   const props =  defineProps(['item']);
   defineEmits(['sizeChanged'])
@@ -13,27 +13,27 @@
   <div>
     <button @click="$emit('sizeChanged', 'h2', props.item )" :class="{ 'bg-tint-1': props.item.headingType == 'h2' }"
     class="hover:bg-tint-1 p-1 rounded-md duration-300">
-        <h2Icon class="fill-tint-8 w-6 h-6" :class="{ 'fill-tint-10': props.item.headingType == 'h2' }" />
+        <H2Icon class="fill-tint-8 w-6 h-6" :class="{ 'fill-tint-10': props.item.headingType == 'h2' }" />
     </button>
 
     <button @click="$emit('sizeChanged', 'h3', props.item )" :class="{ 'bg-tint-1': props.item.headingType == 'h3' }"
     class="hover:bg-tint-1 p-1 rounded-md duration-300">
-        <h3Icon class="fill-tint-8 w-6 h-6" :class="{ 'fill-tint-10': props.item.headingType == 'h3' }"/>
+        <H3Icon class="fill-tint-8 w-6 h-6" :class="{ 'fill-tint-10': props.item.headingType == 'h3' }"/>
     </button> 
 
     <button @click="$emit('sizeChanged', 'h4', props.item )" :class="{ 'bg-tint-1': props.item.headingType == 'h4' }"
     class="hover:bg-tint-1 p-1 rounded-md duration-300">
-        <h4Icon class="fill-tint-8 w-6 h-6" :class="{ 'fill-tint-10': props.item.headingType == 'h4' }"/>
+        <H4Icon class="fill-tint-8 w-6 h-6" :class="{ 'fill-tint-10': props.item.headingType == 'h4' }"/>
     </button>
 
     <button @click="$emit('sizeChanged', 'h5', props.item )" :class="{ 'bg-tint-1': props.item.headingType == 'h5' }"
     class="hover:bg-tint-1 p-1 rounded-md duration-300">
-        <h5Icon class="fill-tint-8 w-6 h-6" :class="{ 'fill-tint-10': props.item.headingType == 'h5' }"/>
+        <H5Icon class="fill-tint-8 w-6 h-6" :class="{ 'fill-tint-10': props.item.headingType == 'h5' }"/>
     </button>
 
     <button @click="$emit('sizeChanged', 'h6', props.item )" :class="{ 'bg-tint-1': props.item.headingType == 'h6' }"
     class="hover:bg-tint-1 p-1 rounded-md duration-300">
-        <h6Icon class="fill-tint-8 w-6 h-6" :class="{ 'fill-tint-10': props.item.headingType == 'h6' }"/>
+        <H6Icon class="fill-tint-8 w-6 h-6" :class="{ 'fill-tint-10': props.item.headingType == 'h6' }"/>
     </button>
   </div>
 </template>
